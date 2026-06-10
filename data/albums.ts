42ee1cce25c32d385328a6f4345729c7e6548b3b
SH-ZMD <sh.zmd01@gmail.com>
2026-06-10 22:24:43 +0800

    Auto publish blog updates 2026-06-10 22:24:43

--- data/albums.ts
+++ data/albums.ts
@@ -2,4 +2,181 @@
 export interface Photo { url: string; caption?: string; }
 export interface Album { id: string; title: string; description: string; cover: string; date: string; photos: Photo[]; }
 
-export const albums: Album[] = [];
+export const albums: Album[] = [
+  {
+    "title": "Minecraft",
+    "cover": "/image/pasted-1781101245276-d06107.png",
+    "id": "album_1781101247443",
+    "photos": [
+      {
+        "url": "/image/pasted-1781101448622-96de1e.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448673-6b9fce.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448719-8f44ef.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448760-2ec894.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448826-c11da3.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448882-b45c23.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448936-afadaf.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101448981-d0aa9b.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449017-a09911.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449041-651302.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449074-a0d956.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449096-01bb04.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449131-41e2ec.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449160-e368c3.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449187-186170.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449227-00e6c2.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449255-214922.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449274-d33aaa.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449294-a1da96.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449316-074eec.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449336-c9ae85.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449367-ac4cf6.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449401-0d0097.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449427-043290.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449450-5034c7.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449494-9a59bd.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449523-939bcf.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449551-01a5e3.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449663-328efb.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449691-182ad3.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449731-0a5548.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449762-747aae.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449811-4bd72f.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449854-a1c371.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449888-ccbc89.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449932-462ae0.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449965-1410a1.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101449995-01a142.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101264772-1e74ef.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101264811-a11d26.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101264832-0a5b42.png",
+        "caption": ""
+      },
+      {
+        "url": "/image/pasted-1781101264844-7f22bb.png",
+        "caption": ""
+      }
+    ],
+    "date": "2026-06-10"
+  }
+];
