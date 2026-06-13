85ee0e3a04c1b9035ee9802de3e17a6fa101bbc4
SH-ZMD <sh.zmd01@gmail.com>
2026-06-13 19:17:21 +0800

    Auto publish blog updates 2026-06-13 19:17:21

--- data/friends.ts
+++ data/friends.ts
@@ -1,4 +1,13 @@
 // 🛡️ 本文件由 XingHuiSama 控制台自动生成
 export interface Friend { id: string; name: string; url: string; description: string; avatar: string; themeColor: string; }
 
-export const friendsData: Friend[] = [];
+export const friendsData: Friend[] = [
+  {
+    "id": "friend_1781349414237",
+    "name": "Foxball",
+    "url": "https://blog.utf-8.fun",
+    "avatar": "/image/fox.png",
+    "description": "只是一只菜鸡狐狸 qaq",
+    "themeColor": "#f264b0"
+  }
+];
